04a1b78cb930fb2a9068bda71fcc4db8f9fa3633
afiffahreza <afiffahreza3@gmail.com>
2022-06-07 03:29:19 +0700

    add db conn

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,39 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+const connection = require("./db");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/", (req, res) =>
+  res.send("Try: /status, /warehouses, or /warehouses/2")
+);
 
+app.get("/status", (req, res) => res.send("Success."));
+
+// app.get('/warehouses', (req, res) => {
+//   connection.query(
+//     "SELECT * FROM `acme`.`warehouses`",
+//     (error, results, fields) => {
+//       if(error) throw error;
+//       res.json(results);
+//     }
+//   );
+// });
+
+// app.route('/warehouses/:id')
+//   .get( (req, res, next) => {
+//     connection.query(
+//       "SELECT * FROM `acme`.`warehouses` WHERE id = ?", req.params.id,
+//       (error, results, fields) => {
+//         if(error) throw error;
+//         res.json(results);
+//       }
+//     );
+//   });
+
+// Use port 8080 by default, unless configured differently in Google Cloud
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App is running at: http://localhost:${port}`);
 });
